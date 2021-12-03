edd191b7ff2d960716fb20225250f9fd97d8b0aa
Iñaki Ulibarri Utrilla <76259599+Inaki-Ulibarri@users.noreply.github.com>
2021-12-03 11:03:10 -0600

    I hate Mariah Carey

---
+++ 3.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+	//file handling shenanigans
+	FILE *f;
+	f = fopen("./input", "r");
+	if(f == NULL){
+		fprintf(stderr,"Error opening the file.\n");
+		exit (1);
+	}
+
+	//get the size of the string being inputed
+	char buff[20];
+	fscanf(f, "%19s[^\n]", buff);
+	size_t foo = strlen(buff);
+
+	struct bit{
+		size_t ones;
+		size_t zero;
+	};
+	struct bit colum[foo];
+	//initialize the the thing to zeros
+	for(size_t i = 0; i<foo; i++){
+		colum[i].ones = 0;
+		colum[i].zero = 0;
+	}
+	fseek(f, 0, SEEK_SET);
+	char ch;
+	size_t leng = 0;
+	
+	while(1){
+		for(size_t i = 0;  i < foo+1; i++){
+			ch = getc(f);
+			switch(ch){
+				case '1':
+					colum[i].ones++;
+					break;
+				case '0':
+					colum[i].zero++;
+					break;
+				case'\n':
+					leng++;
+					break;
+				default:
+					break;
+			}
+		}
+
+		if(feof(f))
+			break;
+	}
+	fclose(f);
+
+	printf("gamma rate: ");
+	for(size_t i = 0; i < foo; i++){
+		if(colum[i].ones < colum[i].zero)
+			printf("0"); 
+		else
+			printf("1");
+	}
+	printf("\n");
+
+	printf("epsilon rate: ");
+	for(size_t i = 0; i < foo; i++){
+		if(colum[i].ones > colum[i].zero)
+			printf("0"); 
+		else
+			printf("1");
+	}
+	printf("\n");
+	
+	return (0);
+}
